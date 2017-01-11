290bae844c03c4fd9d2fdefcfff16568a79cf9f7
Agie123 <ahmetcan_@live.nl>
2017-01-11 10:19:57 +0100

    Date

--- pyfs/src/pyfs/Foundd.java
+++ pyfs/src/pyfs/Foundd.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pyfs;
 
 import java.sql.Connection;
@@ -16,51 +11,78 @@ import java.sql.Statement;
  * @author merij
  */
 public class Foundd {
-    
+
     mysql Mysql = new mysql();
     Found found1 = new Found();
 
     public Foundd() {
     }
-    
+
     public void getLuggage() {
-        
-         Connection conn;                                                            //making connection to database
 
-                final String USERNAME = Mysql.username();
-                final String PASSWORD = Mysql.password();
-                final String CONN_STRING = Mysql.urlmysql();
+        Connection conn;                                                            //making connection to database
 
-                String[] foundbagage = new String[5];
-                foundbagage[0] = found1.getLugtype();
-                foundbagage[1] = found1.getLugbrand();
-                foundbagage[2] = found1.getLugcolor();
-                foundbagage[3] = found1.getLugweight();
-                foundbagage[4] = found1.getLugspef();
+        final String USERNAME = Mysql.username();
+        final String PASSWORD = Mysql.password();
+        final String CONN_STRING = Mysql.urlmysql();
 
-                try {
+        String[] foundbagage = new String[5];
+        foundbagage[0] = found1.getLugtype();
+        foundbagage[1] = found1.getLugbrand();
+        foundbagage[2] = found1.getLugcolor();
+        foundbagage[3] = found1.getLugweight();
+        foundbagage[4] = found1.getLugspef();
 
-                    conn = DriverManager.getConnection(CONN_STRING, USERNAME, PASSWORD);
-                    Statement st = conn.createStatement();
-                    String primkey = "SELECT MAX(Lugagenr) FROM lugage ";
-                    ResultSet rs = st.executeQuery(primkey);
+        try {
 
-                    String query = "INSERT INTO lugage (Lugagetype, Lugagebrand, Lugagecol, Lugageweight, Lugagespef, Lugagenr) VALUES (" + '"' + foundbagage[0] + '"' + "," + '"' + foundbagage[1] + '"' + "," + '"' + foundbagage[2] + '"' + "," + '"'
-                            + foundbagage[3] + '"' + "," + '"' + foundbagage[4] + '"' + "," + '"' + +'"' + " )";
+            conn = DriverManager.getConnection(CONN_STRING, USERNAME, PASSWORD);
+            Statement st = conn.createStatement();
+            String primkey = "SELECT MAX(Lugagenr) FROM lugage ";
+            ResultSet rs = st.executeQuery(primkey);
 
-                    st.executeUpdate(query);
+            String query = "INSERT INTO lugage (Lugagetype, Lugagebrand, Lugagecol, Lugageweight, Lugagespef, Lugagenr) VALUES (" + '"' + foundbagage[0] + '"' + "," + '"' + foundbagage[1] + '"' + "," + '"' + foundbagage[2] + '"' + "," + '"'
+                    + foundbagage[3] + '"' + "," + '"' + foundbagage[4] + '"' + "," + '"' + +'"' + " )";
 
-                } catch (SQLException ed) {
+            st.executeUpdate(query);
 
-                    System.err.println(ed);
+        } catch (SQLException ed) {
 
-                }
-        
-        
-        
-        
+            System.err.println(ed);
+
+        }
+
     }
-    
-    
-    
-}
+
+    public void getDate() {
+
+        Connection conn;                                                            //making connection to database
+
+        final String USERNAME = Mysql.username();
+        final String PASSWORD = Mysql.password();
+        final String CONN_STRING = Mysql.urlmysql();
+        
+        String[] datex = new String[4];
+        datex[0] = found1.getDate().getEditor().getText();
+        datex[1] = found1.Time().getText();
+        datex[2] = found1.getAirport();
+        //datex[3] = found1.get VERGEET UNR NIET
+
+        try {
+
+            conn = DriverManager.getConnection(CONN_STRING, USERNAME, PASSWORD);
+            Statement st = conn.createStatement();
+            
+            
+            
+            String query = "INSERT INTO dta (date, time, airport, Unr) VALUES (?,?,?,?)";
+            st.executeUpdate(query);
+            
+            st.executeUpdate(query);
+        }
+        catch (SQLException ed) {
+
+            System.err.println(ed);
+
+        }
+
+    }}
